49c7fa68cfa9b4911dab57aff9b5422662b77fdc
“jeferson” <jefersonsatoshi@hotmail.com>
2024-12-07 03:48:21 -0300

    Atividade & Database H2

---
+++ SistemaEvento/src/test/java/com/desafio02/SistemaEvento/SistemaEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio02.SistemaEvento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
